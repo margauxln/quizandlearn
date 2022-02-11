a49f662958ffd851776331230e706644256d771f
CamilleLafrance <77630883+CamilleLafrance@users.noreply.github.com>
2022-02-11 16:20:54 +0100

    update modules & add port in server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+// const router = express.Router();
+
 const app = express();
+
 const db = config.get("mongoURI");
 
 mongoose
@@ -11,3 +14,16 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
+//BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+const port = 5000;
+app.listen(port, () =>
+  console.log(`Server started on port: http://localhost:${port}`)
+);
+
+/*
+//Routes
+app.use('/',require('./routes/index'));
+*/
