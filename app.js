ff8062628eaeeb6219648d50deaa8635cf0abb91
Marie Flores <marieflores.contact@gmail.com>
2022-02-18 16:00:09 +0100

    login signup et middleware auth ok

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require("config");
+const userRoutes = require('./routes/user');
 
 const app = express();
+//connection bdd avec mongoose
+const db = config.get("mongoURI");
 
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
 
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
