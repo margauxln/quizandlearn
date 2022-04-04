7c3bc66229aaae1b43036a7992e24b83e0691a29
sbevilacqua <sbevilacqua@omnilog.fr>
2022-04-04 22:10:35 +0200

    feat-US7-started working on answers list

---
+++ frontend/src/pages/QuizCreationRefacto/QuizCreationRefacto.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Formik, Form, Field, FieldArray } from 'formik';
+
+const questionLimit = 5;
+
+export const FriendList = () => (
+  <div>
+    <h1>Friend List</h1>
+    <Formik
+      initialValues={{ 
+          quizTitle: "",
+          quizDescription:"",
+          quizCategory: "",
+          questionTitle: "",
+          otherAnswers: ["firstAnswer", "secondAnwer"], //max 3
+          otherQuestions: [] }} //max 9
+
+      render={({ values }) => (
+        <Form>
+
+            <FieldArray
+                name="otherAnswers"
+                render={arrayHelpers => (
+                <div>
+                    {(values.otherAnswers && values.otherAnswers.length > 0 && values.otherAnswers.length <= questionLimit) && (
+                    values.otherAnswers.map((otherAnswer, index) => (
+                        <div key={index}>
+                        <Field name={`otherAnswers.${index}`} />
+                        <button
+                            type="button"
+                            onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                        >
+                            -
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                        >
+                            +
+                        </button>
+                        </div>
+                        ))
+                    )}
+                    
+
+                    <div>
+                    <button type="submit">Submit</button>
+                    </div>
+                </div>
+                )}
+            />
+        </Form>
+      )}
+    />
+  </div>
+);
