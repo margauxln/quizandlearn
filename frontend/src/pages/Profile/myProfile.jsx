7ef0352672d9c94c65fa680e5b777f0b832cab7a
Alina <67075021+alinabudukina@users.noreply.github.com>
2022-04-08 17:01:26 +0200

    fix: containers css

--- frontend/src/pages/Profile/myProfile.jsx
+++ frontend/src/pages/Profile/myProfile.jsx
@@ -1,10 +1,20 @@
 import "./myProfile.css";
 
 const MyProfile = () => {
-    
-    return(
-        <div>Toto</div>
-    )
-}
+  return (
+    <>
+      <div className="quizCreationPageContainer">
+        <form id="quizCreationFormContainer">
+          <h1 id="titleQuizCreation">My profile</h1>
+          <div className="secondSectionContainer">
+            <label htmlFor="title" className="sr-only">
+              Title
+            </label>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default MyProfile;
